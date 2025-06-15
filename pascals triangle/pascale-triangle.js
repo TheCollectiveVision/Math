f20c7e26dcdc15eefdb7a5befb3e652f783e5875
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2025-06-15 16:49:45 +0800

    Fix Pascal triangle generation

--- pascals triangle/pascale-triangle.js
+++ pascals triangle/pascale-triangle.js
@@ -1,14 +1,24 @@
-let generatePascal = (result,n)=>{
-    let last = result[result.length - 1]
-    let newRow = new Array(last.length + 1)
-    newRow[0] = 1
-    newRow[newRow.length] = 1    
+let generatePascal = (result, n) => {
+    while (result.length < n) {
+        let last = result[result.length - 1];
+        let newRow = new Array(last.length + 1);
+        newRow[0] = 1;
+        newRow[newRow.length - 1] = 1;
+        for (let i = 1; i < last.length; i++) {
+            newRow[i] = last[i - 1] + last[i];
+        }
+        result.push(newRow);
+    }
+    return result;
 }
 
-let pascal = (n)=>{
-    let result = [[1],[1,1]]
-    if(n == 2) return result
-    else return generatePascal(result,n)
+let pascal = (n) => {
+    if (n <= 0) return [];
+    let result = [[1]];
+    if (n === 1) return result;
+    result.push([1, 1]);
+    if (n === 2) return result;
+    return generatePascal(result, n);
 }
 
 
