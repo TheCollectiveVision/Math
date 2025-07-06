a955f260921dac64e5a4537ea7901adb5acb07ec
Arjundevjha <111162691+Arjundevjha@users.noreply.github.com>
2025-07-06 11:11:41 +0800

    Refactor cubic formula implementation to improve root calculation and user prompts

--- Algebra/Polynomials/cubic_formula/main.py
+++ Algebra/Polynomials/cubic_formula/main.py
@@ -1,25 +1,35 @@
 def main():
-    print("This is the main function of the cubic formula module.")
-    print("This program will help you find the roots of a cubic equation of the form ax^3 + bx^2 + cx + d = 0.")
-    a = float(input("Enter the coefficient x^3 (a): "))
-    b = float(input("Enter the coefficient x^2 (b): "))
-    c = float(input("Enter the coefficient x (c): "))
-    d = float(input("Enter the constant term (d): "))
+    print("Ennter in the coefficients of a, b, c, d for the cubic equation ax^3 + bx^2 + cx + d")
+    a = int(input("Coefficient a: "))
+    b = int(input("Coefficient b: "))
+    c = int(input("Coefficient c: "))
+    d = int(input("Coefficient d: "))
+
+    i = (-1) ** 0.5  # i is the imaginary unit
     if a == 0:
         print("Coefficient 'a' cannot be zero for a cubic equation.")
         return
     
-    value1 = (-((b)**3)/(27 * a **3)) + ((b*c)/(6*a**2)) - (d/(2*a))
-    value2 = value1**2 + ((c/(3*a))**3)
-    value3 = value2 ** (1/2)
+    term_1 = -(b/(3*a))
     
-    alpha = (value1 + value3) ** (1/3) 
-    beta = (value1 - value3) ** (1/3)
-    gamma = - b/(3 * a)
+    inner_term_1 = 2*(b**3) - 9*a*b*c + 27*(a**2)*d
+    inner_term_2 = 4*((b**2 - 3*a*c)**3)
     
-    solutions = alpha + beta + gamma
+    cubed_value_1 = 0.5 * (inner_term_1 + (inner_term_2**0.5))
+    cubed_value_2 = 0.5 * (inner_term_1 - (inner_term_2**0.5))
+
+    normal_multiplier = -1/(3*a)
+    complex_multiplier_1 = (1 + (1*i * (3**0.5))) / (6 * a)
+    complex_multiplier_2 = (1 + (-1*i * (3**0.5))) / (6 * a)
     
-    print(f"The solution to the cubic equation {a}x^3 + {b}x^2 + {c}x + {d} = 0 is: {solutions}")
-    
+    x1 = term_1 + ((cubed_value_1**(1/3)) * normal_multiplier) + ((cubed_value_2**(1/3)) * normal_multiplier)
+    x2 = term_1 + ((cubed_value_1**(1/3)) * complex_multiplier_1) + ((cubed_value_2**(1/3)) * complex_multiplier_2)
+    x3 = term_1 + ((cubed_value_1**(1/3)) * complex_multiplier_2) + ((cubed_value_2**(1/3)) * complex_multiplier_1)
+
+    print("The roots of the cubic equation are:")
+    print("x1 =", x1)
+    print("x2 =", x2)
+    print("x3 =", x3)
+
 if __name__ == "__main__":
     main()
