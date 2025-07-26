f0f467ec270561bed4173912faa55916def29293
AaravRastogi-TheChef <aarav.rastogi24@gmail.com>
2025-07-26 18:43:54 +0800

    Create cosine.py

---
+++ Geometry/Trigonometry/cosine.py
@@ -0,0 +1,24 @@
+import sys as s
+s.path.append("../..")
+from Numerical_Methods.Factorial.factorial import factorial
+from Numerical_Methods.Pi_Algorithms.Machin_algo import MachinAlgorithm
+
+def Cosine(radians):
+    cos = 1
+    sign = 1
+    
+    for idx in range(2,100,2):
+        if sign % 2 == 0:
+            cos += radians**idx/factorial(idx)
+        else:
+            cos -= radians**idx/factorial(idx)
+        sign += 1 
+    
+    return cos 
+
+if __name__ == "__main__":
+    precision = 10
+    degree = int(input("Please input angle in degrees: "))
+    radians = degree/180 * float(MachinAlgorithm.calculate_pi(precision))
+    cos = Cosine(radians)
+    print(f'The value of cosine is {cos:.2f}')
