5e69ae2dfd0e1432543702c691cd8e7879914a71
AaravRastogi-TheChef <aarav.rastogi24@gmail.com>
2025-07-26 18:47:43 +0800

    Update sine.py

--- Geometry/Trigonometry/sine.py
+++ Geometry/Trigonometry/sine.py
@@ -1,18 +1,24 @@
 import sys as s
 s.path.append("../..")
 from Numerical_Methods.Factorial.factorial import factorial
-
-
+from Numerical_Methods.Pi_Algorithms.Machin_algo import MachinAlgorithm
 
 def Sine(radians): 
     sine = 0
     sign = 0
         
     for idx in range(1,100,2):
-        if sign % 2 == 0:
+        if sign % 2 == 0:   
             sine += radians**idx/factorial(idx)
         else:
             sine -= radians**idx/factorial(idx)
         sign += 1 
     
     return sine 
+
+if __name__ == "__main__":
+    precision = 10
+    degree = int(input("Please input angle in degrees: "))
+    radians = degree/180 * float(MachinAlgorithm.calculate_pi(precision))
+    sine = Sine(radians)
+    print(f'The value of sine is {sine:.2f}')
